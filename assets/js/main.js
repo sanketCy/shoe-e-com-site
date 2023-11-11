54f15148aa53fb2176861c4f44f25f250ee3dc50
sanket chavan <csaket016@gmail.com>
2023-11-11 13:47:57 +0530

    adding nav menu in side

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+
+showMenu('nav__toggle', 'nav__menu');
 
 /*===== REMOVE MENU =====*/
 
-
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 
-
-/*===== CHANGE COLOR HEADER =====*/ 
+/*===== CHANGE COLOR HEADER =====*/
